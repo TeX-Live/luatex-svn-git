ed7add37ff5f8f14cc290af8cba367031ba95419
Taco Hoekwater <taco@elvenkind.com>
2008-01-26 15:04:32 +0000

    better splitter
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@980 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/web2c/luatexdir/lua/loslibext.c
+++ src/texk/web2c/luatexdir/lua/loslibext.c
@@ -109,63 +109,54 @@ spawn_command(const char *file, char *const *argv, char *const *envp)
 extern char **environ;
 
 static char **
-do_split_command(char *maincmd, char target)
+do_split_command(char *maincmd)
 {
-    char *piece;
+    char *piece, *start_piece;
     char *cmd;
     char **cmdline = NULL;
     unsigned int i, j;
     int ret = 0;
     int in_string = 0;
+    int quoted = 0;
     if (strlen(maincmd) == 0)
 	return NULL;
+    /* allocate the array of options first. it will probably be 
+       be a little bit too big, but better too much than to little */
     j=2;
-    for (i=0; i < strlen(maincmd); i++) {
-      if (maincmd[i]==' ') j++;
-    }
+    for (i = 0; i < strlen(maincmd); i++) { if (maincmd[i]==' ') j++; }
     cmdline = malloc(sizeof(char *) * j);
-    for (i = 0; i < j; i++) {
-       cmdline[i] = NULL;
-    }
-    cmd = strdup(maincmd);
+    for (i = 0; i < j; i++) { cmdline[i] = NULL; }
+    cmd = maincmd;
     i = 0;
-    while (cmd[i] == ' ')
-	i++;
-    piece = cmd;
+    while (cmd[i] == ' ') i++; /* skip leading spaces */
+    start_piece = malloc(strlen(cmd)+1); /* a buffer */
+    piece = start_piece;
     for (; i <= strlen(maincmd); i++) {
-	if (in_string == 1) {
-	    if (cmd[i] == '"') {
-		in_string = 0;
-	    }
-	} else if (in_string == 2) {
-	    if (cmd[i] == '\'') {
-		  in_string = 0;
-	    }
-	} else {
-	    if (cmd[i] == '"') {
-		in_string = 1;
-	    } else if (cmd[i] == '\'') {
-		in_string = 2;
-	    } else if (cmd[i] == target) {
-		cmd[i] = 0;
-		if ((*piece == '"' && *(piece+strlen(piece)-1) == '"')||
-                    (*piece == '\'' && *(piece+strlen(piece)-1) == '\'')) {
-		  *(piece+strlen(piece)-1)=0; piece++;
-		}
-		cmdline[ret++] = strdup(piece);		
-		while (i < strlen(maincmd) && cmd[(i + 1)] == ' ')
-		    i++;
-		piece = cmd + i + 1;
-	    }
-	}
-    }
-    if (*piece) {
-      if ((*piece == '"' && *(piece+strlen(piece)-1) == '"')||
-	  (*piece == '\'' && *(piece+strlen(piece)-1) == '\'')) {
-	*(piece+strlen(piece)-1)=0; piece++;
+      if (cmd[i]=='\\' && 
+	  (cmd[i+1] == '\\' || cmd[i+1] == '\'' || cmd[i+1] == '"')) {
+	quoted =1;
+	continue;
       }
-      cmdline[ret++] = strdup(piece);
+      if (in_string && cmd[i] == in_string  && !quoted) { 
+	in_string = 0; 
+	continue; 
+      } 
+      if ((cmd[i] == '"' || cmd[i] == '\'')&& !quoted) { 
+	in_string = cmd[i]; 
+	continue; 
+      } 
+      if ((in_string==0 && cmd[i] == ' ') || cmd[i]==0) {
+	*piece = 0; 
+	cmdline[ret++] = xstrdup(start_piece);
+	piece = start_piece;
+	while (i < strlen(maincmd) && cmd[(i + 1)] == ' ')
+	  i++;
+	continue;
+      }
+      *piece++ = cmd[i];
+      quoted=0;
     }
+    xfree(start_piece);
     return cmdline;
 }
 
@@ -215,7 +206,7 @@ static int os_exec (lua_State *L) {
     return 0;
   if (lua_type(L,1)==LUA_TSTRING) {
     maincmd =  (char *)lua_tostring(L, 1);
-    cmdline = do_split_command(maincmd, ' ');
+    cmdline = do_split_command(maincmd);
   } else if (lua_type(L,1)==LUA_TTABLE) {
     cmdline = do_flatten_command(L);
   }
@@ -237,7 +228,7 @@ static int os_spawn (lua_State *L) {
   }
   if (lua_type(L,1)==LUA_TSTRING) {
     maincmd =  (char *)lua_tostring(L, 1);
-    cmdline = do_split_command(maincmd, ' ');
+    cmdline = do_split_command(maincmd);
   } else if (lua_type(L,1)==LUA_TTABLE) {
     cmdline = do_flatten_command(L);
   }
