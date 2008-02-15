06ff992192f876eef5eedb802e97934f3aca138d
Hartmut Henkel <hartmut_henkel@gmx.de>
2008-02-15 00:53:12 +0000

    remove stackDump() debugging function
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@1037 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/web2c/luatexdir/image/writeimg.c
+++ src/texk/web2c/luatexdir/image/writeimg.c
@@ -31,37 +31,6 @@ $Id$
 #include <../lua51/lauxlib.h>
 
 /**********************************************************************/
-
-#ifdef DEBUG
-static void stackDump(lua_State * L, char *s)
-{
-    int i;
-    int top = lua_gettop(L);
-    printf("\n=== stackDump <%s>: ", s);
-    for (i = 1; i <= top; i++) {        /* repeat for each level */
-        int t = lua_type(L, i);
-        printf("%d: ", i);
-        switch (t) {
-        case LUA_TSTRING:      /* strings */
-            printf("`%s'", lua_tostring(L, i));
-            break;
-        case LUA_TBOOLEAN:     /* booleans */
-            printf(lua_toboolean(L, i) ? "true" : "false");
-            break;
-        case LUA_TNUMBER:      /* numbers */
-            printf("%g", lua_tonumber(L, i));
-            break;
-        default:               /* other values */
-            printf("%s", lua_typename(L, t));
-            break;
-        }
-        printf("  ");           /* put a separator */
-    }
-    printf("\n");
-}
-#endif
-
-/**********************************************************************/
 /*
   Patch ImageTypeDetection 2003/02/08 by Heiko Oberdiek.
 
