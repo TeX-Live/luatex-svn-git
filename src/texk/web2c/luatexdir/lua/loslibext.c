cfd0a8da9199511f5efed75f6e35ceb166d04fde
Taco Hoekwater <taco@elvenkind.com>
2008-05-22 12:29:54 +0000

    implement os.uname()
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@1281 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/web2c/luatexdir/lua/loslibext.c
+++ src/texk/web2c/luatexdir/lua/loslibext.c
@@ -40,6 +40,7 @@ static const char _svn_version[] =
 #  define OS_PLATTYPE "msdos"
 #  define OS_PLATNAME "msdos"
 #else                           /* assume everything else is unix-y */
+#include <sys/utsname.h>
 #  define OS_PLATTYPE "unix"
 /* this is just a first guess */
 #  if defined(__BSD__)
@@ -511,6 +512,179 @@ static int ex_sleep(lua_State * L)
     return 0;
 }
 
+
+
+#ifdef WIN32
+#define _UTSNAME_LENGTH 65
+
+/* Structure describing the system and machine.  */
+struct utsname
+{
+  char sysname [_UTSNAME_LENGTH];
+  char nodename[_UTSNAME_LENGTH];
+  char release [_UTSNAME_LENGTH];
+  char version [_UTSNAME_LENGTH];
+  char machine [_UTSNAME_LENGTH];
+};
+
+/*
+ * Get name and information about current kernel.
+ */
+static int
+uname (struct utsname *uts)
+{
+  enum { WinNT, Win95, Win98, WinUnknown };
+  OSVERSIONINFO osver;
+  SYSTEM_INFO sysinfo;
+  DWORD sLength;
+  DWORD os = WinUnknown;
+
+  memset (uts, 0, sizeof (*uts));
+
+  osver.dwOSVersionInfoSize = sizeof (osver);
+  GetVersionEx (&osver);
+  GetSystemInfo (&sysinfo);
+
+  switch (osver.dwPlatformId)
+    {
+    case VER_PLATFORM_WIN32_NT: /* NT, Windows 2000 or Windows XP */
+      if (osver.dwMajorVersion == 4)
+        strcpy (uts->sysname, "Windows NT4x"); /* NT4x */
+      else if (osver.dwMajorVersion <= 3)
+        strcpy (uts->sysname, "Windows NT3x"); /* NT3x */
+      else if (osver.dwMajorVersion == 5 ) {
+	if (osver.dwMinorVersion == 0)
+	  strcpy (uts->sysname, "Windows 2000"); /* 2k */
+        else if (osver.dwMinorVersion == 1)
+          strcpy (uts->sysname, "Windows XP");   /* XP */
+        else if (osver.dwMinorVersion == 2)
+          strcpy (uts->sysname, "Windows Server 2003");   /* Server 2003 */
+      } else if (osver.dwMajorVersion == 6) {
+	/*
+	if( osver.wProductType == VER_NT_WORKSTATION )
+	*/
+	  strcpy (uts->sysname, "Windows Vista");   /* Vista */
+	  /*
+  	else
+	  strcpy (uts->sysname, "Windows Server 2008"); 
+	  */
+      }
+      os = WinNT;
+      break;
+
+    case VER_PLATFORM_WIN32_WINDOWS: /* Win95, Win98 or WinME */
+      if ((osver.dwMajorVersion > 4) || 
+          ((osver.dwMajorVersion == 4) && (osver.dwMinorVersion > 0)))
+        {
+	  if (osver.dwMinorVersion >= 90)
+	    strcpy (uts->sysname, "Windows ME"); /* ME */
+	  else
+	    strcpy (uts->sysname, "Windows 98"); /* 98 */
+          os = Win98;
+        }
+      else
+        {
+          strcpy (uts->sysname, "Windows 95"); /* 95 */
+          os = Win95;
+        }
+      break;
+
+    case VER_PLATFORM_WIN32s: /* Windows 3.x */
+      strcpy (uts->sysname, "Windows");
+      break;
+
+    default: /* anything else */
+      strcpy (uts->sysname, "Windows");
+      break;
+    }
+
+  sprintf (uts->version, "%ld.%02ld", 
+           osver.dwMajorVersion, osver.dwMinorVersion);
+
+  if (osver.szCSDVersion[0] != '\0' &&
+      (strlen (osver.szCSDVersion) + strlen (uts->version) + 1) < 
+      sizeof (uts->version))
+    {
+      strcat (uts->version, " ");
+      strcat (uts->version, osver.szCSDVersion);
+    }
+
+  sprintf (uts->release, "build %ld", osver.dwBuildNumber & 0xFFFF);
+
+  switch (sysinfo.wProcessorArchitecture)
+    {
+    case PROCESSOR_ARCHITECTURE_PPC:
+      strcpy (uts->machine, "ppc");
+      break;
+    case PROCESSOR_ARCHITECTURE_ALPHA:
+      strcpy (uts->machine, "alpha");
+      break;
+    case PROCESSOR_ARCHITECTURE_MIPS:
+      strcpy (uts->machine, "mips");
+      break;
+    case PROCESSOR_ARCHITECTURE_INTEL:
+      /* 
+       * dwProcessorType is only valid in Win95 and Win98 and WinME
+       * wProcessorLevel is only valid in WinNT 
+       */
+      switch (os)
+        {
+        case Win95:
+        case Win98:
+          switch (sysinfo.dwProcessorType)
+            {
+            case PROCESSOR_INTEL_386:
+            case PROCESSOR_INTEL_486:
+            case PROCESSOR_INTEL_PENTIUM:
+              sprintf (uts->machine, "i%ld", sysinfo.dwProcessorType);
+              break;
+            default:
+              strcpy (uts->machine, "i386");
+              break;
+          }
+          break;
+        case WinNT:
+          sprintf (uts->machine, "i%d86", sysinfo.wProcessorLevel);
+          break;
+        default:
+          strcpy (uts->machine, "unknown");
+          break;
+      }
+      break;
+    default:
+      strcpy (uts->machine, "unknown");
+      break;
+  }
+  
+  sLength = sizeof (uts->nodename) - 1;
+  GetComputerName (uts->nodename, &sLength);
+  return 0;
+}
+#endif
+
+
+static int ex_uname(lua_State * L)
+{
+    struct utsname uts;
+    if (uname(&uts)>=0) {
+      lua_newtable(L);
+      lua_pushstring(L, uts.sysname);
+      lua_setfield(L,-2,"sysname");
+      lua_pushstring(L, uts.machine);
+      lua_setfield(L,-2,"machine");
+      lua_pushstring(L, uts.release);
+      lua_setfield(L,-2,"release");
+      lua_pushstring(L, uts.version);
+      lua_setfield(L,-2,"version");
+      lua_pushstring(L, uts.nodename);
+      lua_setfield(L,-2,"nodename");
+    } else {
+      lua_pushnil(L);
+    }
+    return 1;
+}
+
+
 #if (! defined (WIN32))  && (! defined (__SUNOS__))
 static int os_times(lua_State * L)
 {
@@ -652,6 +826,9 @@ void open_oslibext(lua_State * L, int safer_option)
     lua_getglobal(L, "os");
     lua_pushliteral(L, OS_PLATNAME);
     lua_setfield(L, -2, "name");
+    lua_getglobal(L, "os");
+    lua_pushcfunction(L, ex_uname);
+    lua_setfield(L, -2, "uname");
 #if (! defined (WIN32))  && (! defined (__SUNOS__))
     lua_getglobal(L, "os");
     lua_pushcfunction(L, os_times);
