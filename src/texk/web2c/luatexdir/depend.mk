f32e637e767f17cad337bca7f07effd092c0f6c3
Martin Schröder <martin@oneiros.de>
2008-07-05 15:19:51 +0000

    rerun depend; --parallel didn't work anymore
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@1365 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/web2c/luatexdir/depend.mk
+++ src/texk/web2c/luatexdir/depend.mk
@@ -2801,6 +2801,57 @@ textcodes.o: ../../../../src/texk/web2c/luatexdir/textcodes.c \
   ../../../../src/texk/web2c/luatexdir/ptexlib.h \
   ../../../../src/texk/web2c/luatexdir/nodes.h \
   ../../../../src/texk/web2c/luatexdir/managed-sa.h
+utils.o: ../../../../src/texk/web2c/luatexdir/utils.c \
+  \
+  \
+  \
+  \
+  \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-proto.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-stat.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/systypes.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-fopen.h \
+  \
+  \
+  ../../../../src/texk/web2c/luatexdir/ptexlib.h ../luatexd.h \
+  ../../../../src/texk/web2c/luatexdir/../texmfmp.h \
+  ../../../../src/texk/web2c/luatexdir/../cpascal.h \
+  ../../../../src/texk/web2c/luatexdir/../config.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/config.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-auto.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-std.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-unistd.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-memstr.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-errno.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-minmax.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-limits.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/debug.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/types.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/lib.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/progname.h \
+  ../../web2c/c-auto.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/getopt.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/proginit.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/tex-file.h \
+  ../../../../src/texk/web2c/luatexdir/../help.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-pathch.h \
+  ../../../../src/texk/web2c/luatexdir/../../kpathsea/c-ctype.h \
+  ../../../../src/texk/web2c/luatexdir/../texmfmem.h ../luatexcoerce.h \
+  ../../../../src/texk/web2c/luatexdir/../luatexdir/luatex.h \
+  ../../../../src/texk/web2c/luatexdir/../luatexdir/ptexlib.h \
+  ../../../../src/texk/web2c/luatexdir/ptexmac.h \
+  ../../../../src/texk/web2c/luatexdir/avlstuff.h \
+  ../../../../src/texk/web2c/luatexdir/avl.h \
+  ../../../../src/texk/web2c/luatexdir/image/image.h \
+  \
+  \
+  \
+  ../../../../src/texk/web2c/luatexdir/font/texfont.h \
+  ../../../../src/texk/web2c/luatexdir/../luatexdir/managed-sa.h \
+  ../../../../src/texk/web2c/luatexdir/hyphen.h \
+  ../../../../src/texk/web2c/luatexdir/nodes.h \
+  \
+ 
 writezip.o: ../../../../src/texk/web2c/luatexdir/writezip.c \
   ../../../../src/texk/web2c/luatexdir/ptexlib.h ../luatexd.h \
   ../../../../src/texk/web2c/luatexdir/../texmfmp.h \
