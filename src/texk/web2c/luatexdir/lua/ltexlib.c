2cb52042c29c80c16b10e069e486e85ff233bfd7
Martin Schröder <martin@oneiros.de>
2008-09-22 16:03:22 +0000

    declare lua_nodelib_push_fast
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@1474 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/web2c/luatexdir/lua/ltexlib.c
+++ src/texk/web2c/luatexdir/lua/ltexlib.c
@@ -26,6 +26,7 @@ static const char _svn_version[] =
     "$Id$ $URL$";
 
 extern halfword *check_isnode(lua_State * L, int ud);
+extern void lua_nodelib_push_fast(lua_State * L, halfword n);
 
 typedef struct {
     char *text;
