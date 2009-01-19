cb9218d0b55ed858fe637adf0cc9337710cdfe22
Hartmut Henkel <hartmut_henkel@gmx.de>
2009-01-19 21:21:44 +0000

    DVI mode: new record type "position" contains h,v:scaled pair. new variable pos; pos.h and pos.v replace page_h and page_v.
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@1785 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/web2c/luatexdir/nodes.h
+++ src/texk/web2c/luatexdir/nodes.h
@@ -547,8 +547,8 @@ typedef enum {
 #  define dir_level(a)     vlink((a)+2)
 #  define dir_dvi_ptr(a)   vinfo((a)+3)
 #  define dir_dvi_h(a)     vlink((a)+3)
-#  define dir_pg_h(a)      vinfo((a)+4)
-#  define dir_pg_v(a)      vlink((a)+4)
+#  define dir_box_pos_h(a) vinfo((a)+4)
+#  define dir_box_pos_v(a) vlink((a)+4)
 #  define dir_cur_h(a)     vinfo((a)+5)
 #  define dir_cur_v(a)     vlink((a)+5)
 
