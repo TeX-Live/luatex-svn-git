db0d02590d8dfd2b2ec77b5d03beef560643e968
Taco Hoekwater <taco@elvenkind.com>
2009-01-23 08:26:02 +0000

    remove the -r switch in the sed call
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@1819 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/web2c/luatexdir/get_svnversion.sh
+++ src/texk/web2c/luatexdir/get_svnversion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 srcdir=$1
 if [ -e "`which svnversion`" ] && [ -e "`which sed`" ]; then
-    REV=`svnversion $srcdir | sed -r 's/^([0-9]+).*/\1/'`
+    REV=`svnversion $srcdir | sed 's/^\([0-9]\+\).*/\1/'`
     case $REV in *[^0-9]*)
         echo unknown
         exit 0
