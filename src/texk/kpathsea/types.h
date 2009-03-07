eaf457ac3a0069d31aef63da307c98cedc748d9b
Taco Hoekwater <taco@elvenkind.com>
2009-03-07 08:42:26 +0000

    make tex-file.h compile standalone again
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@1950 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/kpathsea/types.h
+++ src/texk/kpathsea/types.h
@@ -39,6 +39,8 @@ typedef int boolean;
 #define TRUE true
 #endif /* FALSE */
 
+#include <stdio.h> /* for FILE* */
+
 /* The usual null-terminated string.  */
 typedef char *string;
 
@@ -72,7 +74,110 @@ typedef struct {
 #include <kpathsea/hash.h>
 #include <kpathsea/str-list.h>
 
-#include <kpathsea/tex-file.h>
+/* from old tex-file.h */
+
+/* We put the glyphs first so we don't waste space in an array in
+   tex-glyph.c.  Accompany a new format here with appropriate changes in
+   tex-file.c and kpsewhich.c (the suffix variable).  */
+typedef enum
+{
+  kpse_gf_format,
+  kpse_pk_format,
+  kpse_any_glyph_format,	/* ``any'' meaning gf or pk */
+  kpse_tfm_format, 
+  kpse_afm_format, 
+  kpse_base_format, 
+  kpse_bib_format, 
+  kpse_bst_format, 
+  kpse_cnf_format,
+  kpse_db_format,
+  kpse_fmt_format,
+  kpse_fontmap_format,
+  kpse_mem_format,
+  kpse_mf_format, 
+  kpse_mfpool_format, 
+  kpse_mft_format, 
+  kpse_mp_format, 
+  kpse_mppool_format, 
+  kpse_mpsupport_format,
+  kpse_ocp_format,
+  kpse_ofm_format, 
+  kpse_opl_format,
+  kpse_otp_format,
+  kpse_ovf_format,
+  kpse_ovp_format,
+  kpse_pict_format,
+  kpse_tex_format,
+  kpse_texdoc_format,
+  kpse_texpool_format,
+  kpse_texsource_format,
+  kpse_tex_ps_header_format,
+  kpse_troff_font_format,
+  kpse_type1_format, 
+  kpse_vf_format,
+  kpse_dvips_config_format,
+  kpse_ist_format,
+  kpse_truetype_format,
+  kpse_type42_format,
+  kpse_web2c_format,
+  kpse_program_text_format,
+  kpse_program_binary_format,
+  kpse_miscfonts_format,
+  kpse_web_format,
+  kpse_cweb_format,
+  kpse_enc_format,
+  kpse_cmap_format,
+  kpse_sfd_format,
+  kpse_opentype_format,
+  kpse_pdftex_config_format,
+  kpse_lig_format,
+  kpse_texmfscripts_format,
+  kpse_lua_format,
+  kpse_last_format /* one past last index */
+} kpse_file_format_type;
+
+
+/* Perhaps we could use this for path values themselves; for now, we use
+   it only for the program_enabled_p value.  */
+typedef enum
+{
+  kpse_src_implicit,   /* C initialization to zero */
+  kpse_src_compile,    /* configure/compile-time default */
+  kpse_src_texmf_cnf,  /* texmf.cnf, the kpathsea config file */
+  kpse_src_client_cnf, /* application config file, e.g., config.ps */
+  kpse_src_env,        /* environment variable */
+  kpse_src_x,          /* X Window System resource */
+  kpse_src_cmdline     /* command-line option */
+} kpse_src_type;
+
+
+/* For each file format, we record the following information.  The main
+   thing that is not part of this structure is the environment variable
+   lists. They are used directly in tex-file.c. We could incorporate
+   them here, but it would complicate the code a bit. We could also do
+   it via variable expansion, but not now, maybe not ever:
+   ${PKFONTS-${TEXFONTS-/usr/local/lib/texmf/fonts//}}.  */
+
+typedef struct
+{
+  const_string type;		/* Human-readable description.  */
+  const_string path;		/* The search path to use.  */
+  const_string raw_path;	/* Pre-$~ (but post-default) expansion.  */
+  const_string path_source;	/* Where the path started from.  */
+  const_string override_path;	/* From client environment variable.  */
+  const_string client_path;	/* E.g., from dvips's config.ps.  */
+  const_string cnf_path;	/* From texmf.cnf.  */
+  const_string default_path;	/* If all else fails.  */
+  const_string *suffix;		/* For kpse_find_file to check for/append.  */
+  const_string *alt_suffix;	/* More suffixes to check for.  */
+  boolean suffix_search_only;	/* Only search with a suffix?  */
+  const_string program;		/* ``mktexpk'', etc.  */
+  int          argc;		/* Count of standard arguments. */
+  const_string *argv;		/* Standard arguments to `program'.  */
+  boolean program_enabled_p;	/* Invoke `program'?  */
+  kpse_src_type program_enable_level; /* Who said to invoke `program'.  */
+  boolean binmode;              /* The files must be opened in binary mode. */
+} kpse_format_info_type;
 
 typedef struct kpathsea_instance *kpathsea;
 
