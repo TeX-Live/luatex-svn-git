be2d270bc1f8e9d9cfcd177a41659554936ca720
Martin Schröder <martin@oneiros.de>
2009-03-11 21:58:25 +0000

    add __svn_version
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@1989 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/web2c/luatexdir/pdf/pdfpage.c
+++ src/texk/web2c/luatexdir/pdf/pdfpage.c
@@ -26,6 +26,8 @@
 
 #include "ptexlib.h"
 
+static const char __svn_version[] = "$Id$";
+
 #define lround(a) (long) floor((a) + 0.5)
 #define setpdffloat(a,b,c) {(a).m = (b); (a).e = (c);}
 #define pdf2double(a) ((double) (a).m / exp10[(a).e])
