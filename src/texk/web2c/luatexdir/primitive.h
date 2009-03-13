a6d50a9c4370f0300b37389c4a1a1d39b094a9f9
Martin Schröder <martin@oneiros.de>
2009-03-13 16:35:07 +0000

    svn:keywords
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2011 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- src/texk/web2c/luatexdir/primitive.h
+++ src/texk/web2c/luatexdir/primitive.h
@@ -17,6 +17,8 @@
    You should have received a copy of the GNU General Public License along
    with LuaTeX; if not, see <http://www.gnu.org/licenses/>. */
 
+/* $Id$ */
+
 #ifndef LUATEX_PRIMITIVE_H
 #define LUATEX_PRIMITIVE_H 1
 
