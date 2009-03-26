e39b7fc80e607df3232dfb84351aa1981e02eb37
Taco Hoekwater <taco@elvenkind.com>
2009-03-26 16:06:47 +0000

    this new script brings together the functionality of the previously separate build.sh, build.sh.linux, and build.sh.cross scripts. (but crosscompilation doesn't work, for some reason)
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2111 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- newbuild.sh
+++ newbuild.sh
@@ -1,5 +1,81 @@
-#!/bin/sh
-../source/configure  \
+#!/usr/bin/env bash
+#
+# Public Domain
+#
+# new script to build luatex binaries
+# ----------
+# Options:
+#       --make      : only make, no make distclean; configure
+#       --parallel  : make -j 2 -l 3.0
+#       --nostrip   : do not strip binary
+#       --mingw     : crosscompile for mingw32 from i-386linux
+      
+
+# try to find gnu make; we may need it
+MAKE=make;
+if make -v 2>&1| grep "GNU Make" >/dev/null
+then 
+  echo "Your make is a GNU-make; I will use that"
+elif gmake -v >/dev/null 2>&1
+then
+  MAKE=gmake;
+  echo "You have a GNU-make installed as gmake; I will use that"
+else
+  echo "I can't find a GNU-make; I'll try to use make and hope that works." 
+  echo "If it doesn't, please install GNU-make."
+fi
+
+ONLY_MAKE=FALSE
+STRIP_LUATEX=TRUE
+MINGWCROSS=FALSE
+JOBS_IF_PARALLEL=2
+MAX_LOAD_IF_PARALLEL=3.0
+
+while [ "$1" != "" ] ; do
+  if [ "$1" = "--make" ] ;
+  then ONLY_MAKE=TRUE ;
+  elif [ "$1" = "--nostrip" ] ;
+  then STRIP_LUATEX=FALSE ;
+  elif [ "$1" = "--mingw" ] ;
+  then MINGWCROSS=TRUE ;
+  elif [ "$1" = "--parallel" ] ;
+  then MAKE="$MAKE -j $JOBS_IF_PARALLEL -l $MAX_LOAD_IF_PARALLEL" ;
+  fi ;
+  shift ;
+done
+#
+STRIP=strip
+
+B=newbuild
+CONFHOST=
+
+if [ "$MINGWCROSS" = "TRUE" ]
+then
+  B=newbuild-windows
+  OLDPATH=$PATH
+  PATH=/usr/mingw32/mingw32/bin:$PATH
+  CONFHOST="--target=i386-pc-mingw32"
+fi
+
+# ----------
+# clean up, if needed
+if [ -r "$B"/Makefile -a $ONLY_MAKE = "FALSE" ]
+then
+  rm -rf "$B"
+elif [ ! -r "$B"/Makefile ]
+then
+    ONLY_MAKE=FALSE
+fi
+if [ ! -r "$B" ]
+then
+  mkdir "$B"
+fi
+#
+cd "$B"
+
+if [ "$ONLY_MAKE" = "FALSE" ]
+then
+../source/configure  $CONFHOST \
     --disable-afm2pl    \
     --disable-aleph  \
     --disable-bibtex   \
@@ -60,4 +136,25 @@
     --disable-shared    \
     --disable-largefile \
     || exit 1 
+fi
+
+$MAKE
+
+# go back
+cd ..
+
+if [ "$STRIP_LUATEX" = "TRUE" ] ;
+then
+  $STRIP "$B"/texk/web2c/luatex
+else
+  echo "luatex binary not stripped"
+fi
+
+if [ "$MINGWCROSS" = "TRUE" ]
+then
+  PATH=$OLDPATH
+fi
+
+# show the results
+ls -l "$B"/texk/web2c/luatex
  
