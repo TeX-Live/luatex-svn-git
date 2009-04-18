d94945d93d13379228c91a273432c12533ffc255
Hartmut Henkel <hartmut_henkel@gmx.de>
2009-04-18 12:20:27 +0000

    move mapfile related macros from ptexmac.h to mapfile.h
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2326 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/font/mapfile.h
+++ source/texk/web2c/luatexdir/font/mapfile.h
@@ -21,6 +21,64 @@
 
 #ifndef MAPFILE_H
 #  define MAPFILE_H
+
+#  define F_INCLUDED          0x01
+#  define F_SUBSETTED         0x02
+#  define F_STDT1FONT         0x04
+#  define F_SUBFONT           0x08
+#  define F_TYPE1             0x10
+#  define F_TRUETYPE          0x20
+#  define F_OTF               0x40
+#  define F_CIDKEYED          0x80
+
+#  define set_included(fm)    ((fm)->type |= F_INCLUDED)
+#  define set_subsetted(fm)   ((fm)->type |= F_SUBSETTED)
+#  define set_std_t1font(fm)  ((fm)->type |= F_STDT1FONT)
+#  define set_subfont(fm)     ((fm)->type |= F_SUBFONT)
+#  define set_type1(fm)       ((fm)->type |= F_TYPE1)
+#  define set_truetype(fm)    ((fm)->type |= F_TRUETYPE)
+#  define set_opentype(fm)    ((fm)->type |= F_OTF)
+#  define set_subfont(fm)     ((fm)->type |= F_SUBFONT)
+#  define set_cidkeyed(fm)    ((fm)->type |= F_CIDKEYED)
+
+#  define unset_included(fm)  ((fm)->type &= ~F_INCLUDED)
+#  define unset_subsetted(fm) ((fm)->type &= ~F_SUBSETTED)
+#  define unset_std_t1font(fm)((fm)->type &= ~F_STDT1FONT)
+#  define unset_subfont(fm)   ((fm)->type &= ~F_SUBFONT)
+#  define unset_type1(fm)     ((fm)->type &= ~F_TYPE1)
+#  define unset_truetype(fm)  ((fm)->type &= ~F_TRUETYPE)
+#  define unset_opentype(fm)  ((fm)->type &= ~F_OTF)
+#  define unset_subfont(fm)   ((fm)->type &= ~F_SUBFONT)
+#  define unset_cidkeyed(fm)  ((fm)->type &= ~F_CIDKEYED)
+
+#  define is_included(fm)     (((fm)->type & F_INCLUDED) != 0)
+#  define is_subsetted(fm)    (((fm)->type & F_SUBSETTED) != 0)
+#  define is_std_t1font(fm)   (((fm)->type & F_STDT1FONT) != 0)
+#  define is_subfont(fm)      (((fm)->type & F_SUBFONT) != 0)
+#  define is_type1(fm)        (((fm)->type & F_TYPE1) != 0)
+#  define is_truetype(fm)     (((fm)->type & F_TRUETYPE) != 0)
+#  define is_opentype(fm)     (((fm)->type & F_OTF) != 0)
+#  define is_subfont(fm)      (((fm)->type & F_SUBFONT) != 0)
+#  define is_cidkeyed(fm)     (((fm)->type & F_CIDKEYED) != 0)
+
+#  define fm_slant(fm)        (fm)->slant
+#  define fm_extend(fm)       (fm)->extend
+#  define fm_fontfile(fm)     (fm)->ff_name
+
+#  define is_reencoded(fm)    ((fm)->encname != NULL)
+#  define is_fontfile(fm)     (fm_fontfile(fm) != NULL)
+#  define is_t1fontfile(fm)   (is_fontfile(fm) && is_type1(fm))
+#  define is_builtin(fm)      (!is_fontfile(fm))
+
+#  define LINK_TFM            0x01
+#  define LINK_PS             0x02
+#  define set_tfmlink(fm)     ((fm)->links |= LINK_TFM)
+#  define set_pslink(fm)      ((fm)->links |= LINK_PS)
+#  define unset_tfmlink(fm)   ((fm)->links &= ~LINK_TFM)
+#  define unset_pslink(fm)    ((fm)->links &= ~LINK_PS)
+#  define has_tfmlink(fm)     ((fm)->links & LINK_TFM)
+#  define has_pslink(fm)      ((fm)->links & LINK_PS)
+
 /**********************************************************************/
 
 typedef struct {
@@ -62,5 +120,4 @@ void delete_fm_entry(fm_entry *);
 int avl_do_entry(fm_entry *, int);
 int check_std_t1font(char *s);
 
-/**********************************************************************/
-#endif
+#endif                          /* MAPFILE_H */
--- source/texk/web2c/luatexdir/ptexmac.h
+++ source/texk/web2c/luatexdir/ptexmac.h
@@ -20,8 +20,8 @@
 
 /* $Id$ */
 
-#ifndef PDFTEXMAC
-#  define PDFTEXMAC
+#ifndef PTEXMAC_H
+#  define PTEXMAC_H
 
 /* Not all systems define it. */
 #  ifndef M_PI
@@ -48,21 +48,21 @@
 
 #  define objinfo(n) objtab[n].int0
 
-#  define pdfroom(n) do {                                      \
-    if ((unsigned)(n + pdf_ptr) > (unsigned)pdf_buf_size) {     \
-        if (pdf_os_mode)                                       \
-            zpdf_os_get_os_buf(n);                               \
+#  define pdfroom(n) do {                                    \
+    if ((unsigned)(n + pdf_ptr) > (unsigned)pdf_buf_size) {  \
+        if (pdf_os_mode)                                     \
+            zpdf_os_get_os_buf(n);                           \
         else {                                               \
-            if ((unsigned)(n) > (unsigned)pdf_buf_size)        \
+            if ((unsigned)(n) > (unsigned)pdf_buf_size)      \
                 pdftex_fail("PDF output buffer overflowed"); \
             else                                             \
-                pdf_flush();                                  \
+                pdf_flush();                                 \
         }                                                    \
     }                                                        \
 } while (0)
 
 #  define pdfout(c)  do {   \
-    pdfroom(1);           \
+    pdfroom(1);             \
     pdf_buf[pdf_ptr++] = c; \
 } while (0)
 
@@ -82,64 +82,64 @@
 extern void check_buffer_overflow(int wsize);
 extern void check_pool_overflow(int wsize);
 
-#  define check_buf(size, buf_size)                                     \
-  if ((unsigned)(size) > (unsigned)(buf_size))                          \
-    pdftex_fail("buffer overflow: %d > %d at file %s, line %d",         \
+#  define check_buf(size, buf_size)                                 \
+  if ((unsigned)(size) > (unsigned)(buf_size))                      \
+    pdftex_fail("buffer overflow: %d > %d at file %s, line %d",     \
                 (int)(size), (int)(buf_size), __FILE__,  __LINE__ )
 
-#  define append_char_to_buf(c, p, buf, buf_size) do {       \
-    if (c == 9)                                            \
-        c = 32;                                            \
-    if (c == 13 || c == EOF)                               \
-        c = 10;                                            \
-    if (c != ' ' || (p > buf && p[-1] != 32)) {            \
-        check_buf(p - buf + 1, (buf_size));                \
-        *p++ = c;                                          \
-    }                                                      \
+#  define append_char_to_buf(c, p, buf, buf_size) do { \
+    if (c == 9)                                        \
+        c = 32;                                        \
+    if (c == 13 || c == EOF)                           \
+        c = 10;                                        \
+    if (c != ' ' || (p > buf && p[-1] != 32)) {        \
+        check_buf(p - buf + 1, (buf_size));            \
+        *p++ = c;                                      \
+    }                                                  \
 } while (0)
 
-#  define append_eol(p, buf, buf_size) do {                  \
-    check_buf(p - buf + 2, (buf_size));                    \
-    if (p - buf > 1 && p[-1] != 10)                        \
-        *p++ = 10;                                         \
-    if (p - buf > 2 && p[-2] == 32) {                      \
-        p[-2] = 10;                                        \
-        p--;                                               \
-    }                                                      \
-    *p = 0;                                                \
+#  define append_eol(p, buf, buf_size) do {            \
+    check_buf(p - buf + 2, (buf_size));                \
+    if (p - buf > 1 && p[-1] != 10)                    \
+        *p++ = 10;                                     \
+    if (p - buf > 2 && p[-2] == 32) {                  \
+        p[-2] = 10;                                    \
+        p--;                                           \
+    }                                                  \
+    *p = 0;                                            \
 } while (0)
 
-#  define remove_eol(p, buf) do {                            \
-    p = strend(buf) - 1;                                   \
-    if (*p == 10)                                          \
-        *p = 0;                                            \
+#  define remove_eol(p, buf) do {                      \
+    p = strend(buf) - 1;                               \
+    if (*p == 10)                                      \
+        *p = 0;                                        \
 } while (0)
 
 #  define skip(p, c)   if (*p == c)  p++
 
-#  define alloc_array(T, n, s) do {                           \
-    if (T##_array == NULL) {                                \
-        T##_limit = (s);                                    \
-        if ((unsigned)(n) > T##_limit)                      \
-            T##_limit = (n);                                \
-        T##_array = xtalloc(T##_limit, T##_entry);          \
-        T##_ptr = T##_array;                                \
-    }                                                       \
+#  define alloc_array(T, n, s) do {                    \
+    if (T##_array == NULL) {                           \
+        T##_limit = (s);                               \
+        if ((unsigned)(n) > T##_limit)                 \
+            T##_limit = (n);                           \
+        T##_array = xtalloc(T##_limit, T##_entry);     \
+        T##_ptr = T##_array;                           \
+    }                                                  \
     else if ((unsigned)(T##_ptr - T##_array + (n)) > (unsigned)(T##_limit)) { \
-        last_ptr_index = T##_ptr - T##_array;               \
-        T##_limit *= 2;                                     \
+        last_ptr_index = T##_ptr - T##_array;          \
+        T##_limit *= 2;                                \
         if ((unsigned)(T##_ptr - T##_array + (n)) > (unsigned)(T##_limit)) \
-            T##_limit = T##_ptr - T##_array + (n);          \
-        xretalloc(T##_array, T##_limit, T##_entry);         \
-        T##_ptr = T##_array + last_ptr_index;               \
-    }                                                       \
+            T##_limit = T##_ptr - T##_array + (n);     \
+        xretalloc(T##_array, T##_limit, T##_entry);    \
+        T##_ptr = T##_array + last_ptr_index;          \
+    }                                                  \
 } while (0)
 
 #  define is_cfg_comment(c) \
     (c == 10 || c == '*' || c == '#' || c == ';' || c == '%')
 
-#  define define_array(T)                     \
-T##_entry      *T##_ptr, *T##_array = NULL;    \
+#  define define_array(T)                   \
+T##_entry      *T##_ptr, *T##_array = NULL; \
 size_t          T##_limit
 
 #  define xfree(p)            do { if (p != NULL) free(p); p = NULL; } while (0)
@@ -163,74 +163,16 @@ size_t          T##_limit
 #  define INT_KEYS_NUM        (FONTBBOX4_CODE + 1)
 #  define FONT_KEYS_NUM       (FONTNAME_CODE + 1)
 
-#  define F_INCLUDED          0x01
-#  define F_SUBSETTED         0x02
-#  define F_STDT1FONT         0x04
-#  define F_SUBFONT           0x08
-#  define F_TYPE1             0x10
-#  define F_TRUETYPE          0x20
-#  define F_OTF               0x40
-#  define F_CIDKEYED          0x80
-
-#  define set_included(fm)    ((fm)->type |= F_INCLUDED)
-#  define set_subsetted(fm)   ((fm)->type |= F_SUBSETTED)
-#  define set_std_t1font(fm)  ((fm)->type |= F_STDT1FONT)
-#  define set_subfont(fm)     ((fm)->type |= F_SUBFONT)
-#  define set_type1(fm)       ((fm)->type |= F_TYPE1)
-#  define set_truetype(fm)    ((fm)->type |= F_TRUETYPE)
-#  define set_opentype(fm)    ((fm)->type |= F_OTF)
-#  define set_subfont(fm)     ((fm)->type |= F_SUBFONT)
-#  define set_cidkeyed(fm)    ((fm)->type |= F_CIDKEYED)
-
-#  define unset_included(fm)  ((fm)->type &= ~F_INCLUDED)
-#  define unset_subsetted(fm) ((fm)->type &= ~F_SUBSETTED)
-#  define unset_std_t1font(fm)((fm)->type &= ~F_STDT1FONT)
-#  define unset_subfont(fm)   ((fm)->type &= ~F_SUBFONT)
-#  define unset_type1(fm)     ((fm)->type &= ~F_TYPE1)
-#  define unset_truetype(fm)  ((fm)->type &= ~F_TRUETYPE)
-#  define unset_opentype(fm)  ((fm)->type &= ~F_OTF)
-#  define unset_subfont(fm)   ((fm)->type &= ~F_SUBFONT)
-#  define unset_cidkeyed(fm)  ((fm)->type &= ~F_CIDKEYED)
-
-#  define is_included(fm)     (((fm)->type & F_INCLUDED) != 0)
-#  define is_subsetted(fm)    (((fm)->type & F_SUBSETTED) != 0)
-#  define is_std_t1font(fm)   (((fm)->type & F_STDT1FONT) != 0)
-#  define is_subfont(fm)      (((fm)->type & F_SUBFONT) != 0)
-#  define is_type1(fm)        (((fm)->type & F_TYPE1) != 0)
-#  define is_truetype(fm)     (((fm)->type & F_TRUETYPE) != 0)
-#  define is_opentype(fm)     (((fm)->type & F_OTF) != 0)
-#  define is_subfont(fm)      (((fm)->type & F_SUBFONT) != 0)
-#  define is_cidkeyed(fm)     (((fm)->type & F_CIDKEYED) != 0)
-
-#  define fm_slant(fm)        (fm)->slant
-#  define fm_extend(fm)       (fm)->extend
-#  define fm_fontfile(fm)     (fm)->ff_name
-
-#  define is_reencoded(fm)    ((fm)->encname != NULL)
-#  define is_fontfile(fm)     (fm_fontfile(fm) != NULL)
-#  define is_t1fontfile(fm)   (is_fontfile(fm) && is_type1(fm))
-#  define is_builtin(fm)      (!is_fontfile(fm))
-
-#  define LINK_TFM            0x01
-#  define LINK_PS             0x02
-#  define set_tfmlink(fm)     ((fm)->links |= LINK_TFM)
-#  define set_pslink(fm)      ((fm)->links |= LINK_PS)
-#  define unset_tfmlink(fm)   ((fm)->links &= ~LINK_TFM)
-#  define unset_pslink(fm)    ((fm)->links &= ~LINK_PS)
-#  define has_tfmlink(fm)     ((fm)->links & LINK_TFM)
-#  define has_pslink(fm)      ((fm)->links & LINK_PS)
-
-
-#  define set_cur_file_name(s)      \
-    cur_file_name = s;      \
+#  define set_cur_file_name(s) \
+    cur_file_name = s;         \
     pack_file_name(maketexstring(cur_file_name), get_nullstr(), get_nullstr())
 
 #  define cmp_return(a, b) \
     if ((a) > (b))         \
-        return 1;        \
+        return 1;          \
     if ((a) < (b))         \
         return -1
 
 #  define str_prefix(s1, s2)  (strncmp((s1), (s2), strlen(s2)) == 0)
 
-#endif                          /* PDFTEXMAC */
+#endif                          /* PTEXMAC_H */
