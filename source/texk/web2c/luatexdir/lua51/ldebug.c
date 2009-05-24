5944718b0409fbcc4ae0b34910152036f7025649
Taco Hoekwater <taco@elvenkind.com>
2009-05-24 08:35:45 +0000

    more getline problems
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2399 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/lua51/ldebug.c
+++ source/texk/web2c/luatexdir/lua51/ldebug.c
@@ -48,7 +48,7 @@ static int currentline (lua_State *L, CallInfo *ci) {
   if (pc < 0)
     return -1;  /* only active lua functions have current-line information */
   else
-    return getline(ci_func(ci)->l.p, pc);
+    return GETLINE(ci_func(ci)->l.p, pc);
 }
 
 
