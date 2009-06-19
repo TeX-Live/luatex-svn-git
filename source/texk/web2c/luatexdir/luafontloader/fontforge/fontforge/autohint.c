56b61f4785aa74333fb511f40f54bf154fd4f58e
Martin Schröder <martin@oneiros.de>
2009-06-19 20:04:58 +0000

    unused variables
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2539 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luafontloader/fontforge/fontforge/autohint.c
+++ source/texk/web2c/luatexdir/luafontloader/fontforge/fontforge/autohint.c
@@ -94,7 +94,9 @@ void FindBlues( SplineFont *sf, int layer, real blues[14], real otherblues[10]) 
     for ( i=0; i<sf->glyphcnt; ++i ) {
 	if ( sf->glyphs[i]!=NULL && sf->glyphs[i]->layers[layer].splines!=NULL ) {
 	    int enc = sf->glyphs[i]->unicodeenc;
+#ifndef LUA_FF_LIB
 	    const unichar_t *upt;
+#endif
 	    if ( enc<0x10000 && isalnum(enc) &&
 		    ((enc>=32 && enc<128 ) || enc == 0xfe || enc==0xf0 || enc==0xdf ||
 		      enc==0x131 ||
@@ -258,7 +260,9 @@ void FindBlues( SplineFont *sf, int layer, real blues[14], real otherblues[10]) 
     base[3] = base[4] = 0;
     for ( i=0; i<sf->glyphcnt; ++i ) if ( sf->glyphs[i]!=NULL ) {
 	int enc = sf->glyphs[i]->unicodeenc;
+#ifndef LUA_FF_LIB
 	const unichar_t *upt;
+#endif
 	if ( enc<0x10000 && isalnum(enc) &&
 		((enc>=32 && enc<128 ) || enc == 0xfe || enc==0xf0 || enc==0xdf ||
 		 (enc>=0x391 && enc<=0x3f3 ) ||
