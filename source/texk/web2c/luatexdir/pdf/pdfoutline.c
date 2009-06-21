5ecbb5a52321b6638fa0cef4a48227ee458e034a
Taco Hoekwater <taco@elvenkind.com>
2009-06-21 13:38:20 +0000

    create and extract a function called scan_pdfoutline()
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2560 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/pdf/pdfoutline.c
+++ source/texk/web2c/luatexdir/pdf/pdfoutline.c
@@ -18,6 +18,7 @@
    with LuaTeX; if not, see <http://www.gnu.org/licenses/>. */
 
 #include "ptexlib.h"
+#include "luatex-api.h"
 
 static const char __svn_version[] =
     "$Id$"
@@ -48,4 +49,79 @@ integer open_subentries(halfword p)
     else
         obj_outline_count(p) = -k;
     return k;
+}
+
+void scan_pdfoutline (void) 
+{
+  halfword p, q, r;
+  integer i, j, k;
+  if (scan_keyword("attr")) {
+    scan_pdf_ext_toks();
+    r = def_ref;
+  } else {
+    r = 0;
+  }
+  p = scan_action();
+  if (scan_keyword("count")) {
+    scan_int();
+    i = cur_val;
+  } else {
+    i = 0;
+  }
+  scan_pdf_ext_toks();
+  q = def_ref;
+  pdf_new_obj(obj_type_others, 0, 1);
+  j = obj_ptr;
+  write_action(p);
+  pdf_end_obj();
+  delete_action_ref(p);
+  pdf_create_obj(obj_type_outline, 0);
+  k = obj_ptr;
+  set_obj_outline_ptr(k, pdf_get_mem(pdfmem_outline_size));
+  set_obj_outline_action_objnum(k,j);
+  set_obj_outline_count(k, i);
+  pdf_new_obj(obj_type_others, 0, 1);
+  {
+    char *s = tokenlist_to_cstring(q, true, NULL);
+    pdf_print_str_ln(s);
+    xfree(s);
+  }
+  delete_token_ref(q);
+  pdf_end_obj();
+  set_obj_outline_title(k, obj_ptr);
+  set_obj_outline_prev(k, 0);
+  set_obj_outline_next(k, 0);
+  set_obj_outline_first(k, 0);
+  set_obj_outline_last(k, 0);
+  set_obj_outline_parent(k, pdf_parent_outline);
+  set_obj_outline_attr(k, r);
+  if (pdf_first_outline == 0)
+    pdf_first_outline =  k;
+  if (pdf_last_outline == 0) {
+    if (pdf_parent_outline != 0)
+      set_obj_outline_first(pdf_parent_outline, k);
+  } else {
+    set_obj_outline_next(pdf_last_outline, k);
+    set_obj_outline_prev(k, pdf_last_outline);
+  }
+  pdf_last_outline = k;
+  if (obj_outline_count(k) != 0) {
+    pdf_parent_outline = k;
+    pdf_last_outline = 0;
+  } else if ((pdf_parent_outline != 0) &&
+	     (outline_list_count(k) == abs(obj_outline_count(pdf_parent_outline)))) {
+    j = pdf_last_outline;
+    do {
+      set_obj_outline_last(pdf_parent_outline, j);
+      j = pdf_parent_outline;
+      pdf_parent_outline = obj_outline_parent(pdf_parent_outline);
+    } while (!((pdf_parent_outline == 0) ||
+	       (outline_list_count(j) < abs(obj_outline_count(pdf_parent_outline)))));
+    if (pdf_parent_outline == 0)
+      pdf_last_outline = pdf_first_outline;
+    else
+      pdf_last_outline = obj_outline_first(pdf_parent_outline);
+    while (obj_outline_next(pdf_last_outline) != 0)
+      pdf_last_outline = obj_outline_next(pdf_last_outline);
+  }
 }
