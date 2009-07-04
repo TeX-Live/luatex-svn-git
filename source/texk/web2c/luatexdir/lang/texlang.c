2f34a964295eeddcbf39438101b484c07b20aee4
Taco Hoekwater <taco@elvenkind.com>
2009-07-04 15:40:20 +0000

    move the last of section 42 to C
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2736 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/lang/texlang.c
+++ source/texk/web2c/luatexdir/lang/texlang.c
@@ -23,8 +23,7 @@
 #include <string.h>
 
 #include "nodes.h"
-#include "font/texfont.h"
-#include "hyphen.h"
+#include "commands.h"
 
 static const char _svn_version[] =
     "$Id$ $URL: http://scm.foundry.supelec.fr/svn/luatex/trunk/src/texk/web2c/luatexdir/lang/texlang.c $";
@@ -1034,3 +1033,68 @@ void undump_language_data(void)
         }
     }
 }
+
+/*
+When \TeX\ has scanned `\.{\\hyphenation}', it calls on a procedure named
+|new_hyph_exceptions| to do the right thing.
+*/
+
+void new_hyph_exceptions (void) /* enters new exceptions */
+{
+    (void)scan_toks(false,true);
+    load_tex_hyphenation(int_par(param_language_code),def_ref);
+    flush_list(def_ref);
+}
+
+/*
+Similarly, when \TeX\ has scanned `\.{\\patterns}', it calls on a
+procedure named |new_patterns|.
+*/
+
+void new_patterns (void) /* initializes the hyphenation pattern data */
+{ 
+    (void)scan_toks(false,true);
+    load_tex_patterns(int_par(param_language_code),def_ref);
+    flush_list(def_ref);
+}
+
+/*
+`\.{\\prehyphenchar}', sets the |pre_break| character, and
+`\.{\\posthyphenchar}' the |post_break| character. Their respective
+defaults are ascii hyphen ("-") and zero (nul).
+*/
+
+void new_pre_hyphen_char (void)
+{
+    scan_optional_equals();
+    scan_int();
+    set_pre_hyphen_char(int_par(param_language_code),cur_val);
+}
+
+void new_post_hyphen_char (void)
+{
+    scan_optional_equals(); 
+    scan_int();
+    set_post_hyphen_char(int_par(param_language_code),cur_val);
+}
+
+/*
+`\.{\\preexhyphenchar}', sets the |pre_break| character, and
+`\.{\\postexhyphenchar}' the |post_break| character. Their
+defaults are both zero (nul).
+*/
+
+void new_pre_exhyphen_char (void)
+{
+    scan_optional_equals(); 
+    scan_int();
+    set_pre_exhyphen_char(int_par(param_language_code),cur_val);
+}
+
+void new_post_exhyphen_char (void)
+{
+    scan_optional_equals(); 
+    scan_int();
+    set_post_exhyphen_char(int_par(param_language_code),cur_val);
+}
+
