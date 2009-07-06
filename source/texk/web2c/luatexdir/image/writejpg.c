40577fc906d41f6a71398d828854f66f3b071474
Martin Schröder <martin@oneiros.de>
2009-07-06 14:41:49 +0000

    handle jfifs where either xres or yres is 0 and the other res is != 0
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2753 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/image/writejpg.c
+++ source/texk/web2c/luatexdir/image/writejpg.c
@@ -157,6 +157,13 @@ void read_jpg_info(PDF pdf, image_dict * idict, img_readtype_e readtype)
                 break;
             }
         }
+        /* if either xres or yres is 0 but the other isn't, set it to the value of the other */
+        if ((img_xres(img) == 0) && (img_yres(img) != 0)) {
+            img_xres(img) = img_yres(img);
+        }
+        if ((img_yres(img) == 0) && (img_xres(img) != 0)) {
+            img_yres(img) = img_xres(img);
+        }
     }
     xfseek(img_file(idict), 0, SEEK_SET, img_filepath(idict));
     while (1) {
