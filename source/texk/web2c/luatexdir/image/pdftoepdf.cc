acac16870a036a098e9061a7e742c0ead9286532
Martin Schröder <martin@oneiros.de>
2009-08-19 13:19:30 +0000

    remove dummy declarations
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@3029 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/image/pdftoepdf.cc
+++ source/texk/web2c/luatexdir/image/pdftoepdf.cc
@@ -141,9 +141,8 @@ struct PdfDocument {
 
 /* AVL sort PdfDocument into PdfDocumentTree by file_path */
 
-static int CompPdfDocument(const void *pa, const void *pb, void *p)
+static int CompPdfDocument(const void *pa, const void *pb, void * /*p */ )
 {
-   (void)p;
     return strcmp(((const PdfDocument *) pa)->file_path,
                   ((const PdfDocument *) pb)->file_path);
 }
@@ -1037,10 +1036,9 @@ static void deletePdfDocument(PdfDocument * pdf_doc)
     delete pdf_doc;
 }
 
-static void destroyPdfDocument(void *pa, void *pb)
+static void destroyPdfDocument(void *pa, void * /*pb */ )
 {
     PdfDocument *pdf_doc = (PdfDocument *) pa;
-    (void)pb;
     deletePdfDocument(pdf_doc);
 }
 
