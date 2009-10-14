f9e69bf70d5952bdfb6177e7eaa71bb0b60af4cb
Taco Hoekwater <taco@elvenkind.com>
2009-10-14 08:46:36 +0000

    bump the version number
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@3061 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -13,10 +13,10 @@
 
 #define TeX
 
-int luatex_version = 43;        /* \.{\\luatexversion}  */
+int luatex_version = 44;        /* \.{\\luatexversion}  */
 int luatex_revision = '0';      /* \.{\\luatexrevision}  */
 int luatex_date_info = -extra_version_info;     /* the compile date is negated */
-char *luatex_version_string = "beta-0.43.0";
+char *luatex_version_string = "beta-0.44.0";
 char *engine_name = "luatex";   /* the name of this engine */
 
 
