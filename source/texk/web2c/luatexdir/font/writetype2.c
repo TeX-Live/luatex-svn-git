273ad85bd529a8510ea47585af13b31dd4400db0
Martin Schröder <martin@oneiros.de>
2010-01-27 13:24:32 +0000

    add missing include
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@3397 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/font/writetype2.c
+++ source/texk/web2c/luatexdir/font/writetype2.c
@@ -1,6 +1,6 @@
 /* writetype0.c
    
-   Copyright 2006-2008 Taco Hoekwater <taco@luatex.org>
+   Copyright 2006-2010 Taco Hoekwater <taco@luatex.org>
 
    This file is part of LuaTeX.
 
@@ -20,6 +20,7 @@
 #include "ptexlib.h"
 #include "writettf.h"
 #include "writecff.h"
+#include "lua/luatex-api.h"
 
 #include "sfnt.h"
 #include "tt_glyf.h"
