43901b60cc4f1f18c744a2d2ec1152fa359d2094
Taco Hoekwater <taco@elvenkind.com>
2010-03-01 19:41:16 +0000

    a patch for getluatexsvnversion.sh to work under dash too, from Elie Roux
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@3450 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/getluatexsvnversion.sh
+++ source/texk/web2c/luatexdir/getluatexsvnversion.sh
@@ -7,12 +7,12 @@ $DEBUG
 FILE="texk/web2c/luatexdir/luatex_svnversion.h"
 
 LANG=C
-if [[ -x `which svnversion` && -d ./.svn ]]
+if ([ -x `which svnversion` ] && [ -d ./.svn ])
 then
   svn up > /dev/null
   svnversion -c . | sed -ne 's/^[0-9]*:*\([0-9]*\).*/#define luatex_svn_revision \1/p' > $FILE
 else
-   if [[ ! -r $FILE ]]
+   if [ ! -r $FILE ]
    then
      echo '#define luatex_svn_revision -1' > $FILE
    fi
