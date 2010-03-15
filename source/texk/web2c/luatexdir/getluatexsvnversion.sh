dc43e61eca76896f8479f6813ff6a8b16d211cca
Taco Hoekwater <taco@elvenkind.com>
2010-03-15 11:57:49 +0000

    new version from Tomek: * no svn up * workaround for a mingw problem * no header file generation if nothing has changed
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@3494 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/getluatexsvnversion.sh
+++ source/texk/web2c/luatexdir/getluatexsvnversion.sh
@@ -7,13 +7,14 @@ $DEBUG
 FILE="texk/web2c/luatexdir/luatex_svnversion.h"
 
 LANG=C
-if ([ -x `which svnversion` ] && [ -d ./.svn ])
+if [ ! -r $FILE ]
 then
-  svn up > /dev/null
-  svnversion -c . | sed -ne 's/^[0-9]*:*\([0-9]*\).*/#define luatex_svn_revision \1/p' > $FILE
-else
-   if [ ! -r $FILE ]
-   then
-     echo '#define luatex_svn_revision -1' > $FILE
-   fi
+  echo '#define luatex_svn_revision -1' > $FILE
+fi
+if ( [ -d ./.svn ] && svnversion > /dev/null )
+then
+  # svn up > /dev/null
+  DEFREV=`cat $FILE`
+  SVNREV=`svnversion -c . | sed -ne 's/^[0-9]*:*\([0-9]*\).*/#define luatex_svn_revision \1/p'`
+  test "$DEFREV" != "$SVNREV" && echo "$SVNREV" > $FILE
 fi
