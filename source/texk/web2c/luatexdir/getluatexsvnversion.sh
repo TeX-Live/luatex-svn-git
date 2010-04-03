3ac09b5cf195d8d4b207d10dfde3da63ecb9d913
Taco Hoekwater <taco@elvenkind.com>
2010-04-03 12:18:15 +0000

    previous patch was broken
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@3586 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/getluatexsvnversion.sh
+++ source/texk/web2c/luatexdir/getluatexsvnversion.sh
@@ -20,6 +20,6 @@ then
 elif ( [ -f ../.git/refs/remotes/git-svn ] && git svn --version > /dev/null )
 then
   DEFREV=`cat $FILE`
-  SVNREV=`git svn info | sed -ne 's/^Revision: \([0-9]*\).*$/#define luatex_svn
+  SVNREV=`git svn info | sed -ne 's/^Revision: \([0-9]*\).*$/#define luatex_svn_revision \1/p'`
   test "$DEFREV" != "$SVNREV" && echo "$SVNREV" > $FILE
 fi
