ce9ca635fadf76395d3766cfc2e1a1513b70ec58
Taco Hoekwater <taco@elvenkind.com>
2010-06-04 12:33:50 +0000

    add kpathsea/concatn.h header, remove superfluous typecasts
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/0.60.x@3714 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -33,6 +33,10 @@ const char *engine_name = "luatex";     /* the name of this engine */
 #include <kpathsea/variable.h>
 #include <kpathsea/absolute.h>
 #include <kpathsea/recorder.h>
+#ifdef WIN32
+#include <kpathsea/concatn.h>
+#endif
+
 
 #include <time.h>               /* For `struct tm'.  */
 #if defined (HAVE_SYS_TIME_H)
@@ -359,16 +363,16 @@ int shell_cmd_is_allowed(const char **cmd, char **safecmd, char **cmdname)
               while (*r && !Isspace(*r))
                 r++;
               if (*r == '\0')
-                q = (char *) concatn ("\"", p, "/", *safecmd, "\"", NULL);
+                q = concatn ("\"", p, "/", *safecmd, "\"", NULL);
               else {
                 *r = '\0';
                 r++;
                 while (*r && Isspace(*r))
                   r++;
                 if (*r)
-                  q = (char *) concatn ("\"", p, "/", *safecmd, "\" ", r, NULL);
+                  q = concatn ("\"", p, "/", *safecmd, "\" ", r, NULL);
                 else
-                  q = (char *) concatn ("\"", p, "/", *safecmd, "\"", NULL);
+                  q = concatn ("\"", p, "/", *safecmd, "\"", NULL);
               }
               free (p);
               free (*safecmd);
