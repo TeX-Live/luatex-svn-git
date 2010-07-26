48a989937627290c1ee7ea8e7b2d8f3936ebb242
Hartmut Henkel <hartmut_henkel@gmx.de>
2010-07-26 20:06:24 +0000

    pdf.pageref() should give error upon neg. page numbers
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@3748 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/lua/lpdflib.c
+++ source/texk/web2c/luatexdir/lua/lpdflib.c
@@ -705,6 +705,8 @@ static int l_pageref(lua_State * L)
     if (n != 1)
         luaL_error(L, "pdf.pageref() needs exactly 1 argument");
     n = (int) luaL_checkinteger(L, 1);
+    if (n <= 0)
+        luaL_error(L, "pdf.pageref() needs page number > 0");
     n = get_obj(static_pdf, obj_type_page, n, false);
     lua_pushnumber(L, n);
     return 1;
