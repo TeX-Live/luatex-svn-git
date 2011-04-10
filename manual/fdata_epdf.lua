14ba82b2d9da0406e106954449c08d2a6c2fbf01
Hartmut Henkel <hartmut_henkel@gmx.de>
2011-04-10 12:39:33 +0000

    sort out epdf methods for functionref.tex
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@4105 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- manual/fdata_epdf.lua
+++ manual/fdata_epdf.lua
@@ -77,6 +77,157 @@ local fdata_epdf = {
     },
   },
   methods = {
+    Annot = {
+      isOK = {
+        type = "function",
+        shortdesc = "Check if Annot object is ok.",
+        arguments = {
+          {type = "Annot", name = "annot", optional = false, },
+        },
+        returnvalues = {
+          {type = "boolean", name = "var", optional = false, },
+        },
+      },
+      getAppearance = {
+        type = "function",
+        shortdesc = "Get Appearance object.",
+        arguments = {
+          {type = "Annot", name = "annot", optional = false, },
+        },
+        returnvalues = {
+          {type = "Object", name = "var", optional = false, },
+        },
+      },
+      getBorder = {
+        type = "function",
+        shortdesc = "Get AnnotBorder object.",
+        arguments = {
+          {type = "Annot", name = "annot", optional = false, },
+        },
+        returnvalues = {
+          {type = "AnnotBorder", name = "var", optional = false, },
+        },
+      },
+      match = {
+        type = "function",
+        shortdesc = "Check if object number and generation matches Ref.",
+        arguments = {
+          {type = "Annot", name = "annot", optional = false, },
+          {type = "Ref", name = "ref", optional = false, },
+        },
+        returnvalues = {
+          {type = "boolean", name = "var", optional = false, },
+        },
+      },
+    },
+    AnnotBorderStyle = {
+      getWidth = {
+        type = "function",
+        shortdesc = "Get border width.",
+        arguments = {
+          {type = "AnnotBorderStyle", name = "annotborderstyle", optional = false, },
+        },
+        returnvalues = {
+          {type = "number", name = "var", optional = false, },
+        },
+      },
+    },
+    Annots = {
+      getNumAnnots = {
+        type = "function",
+        shortdesc = "Get number of Annots objects.",
+        arguments = {
+          {type = "Annots", name = "annots", optional = false, },
+        },
+        returnvalues = {
+          {type = "integer", name = "var", optional = false, },
+        },
+      },
+      getAnnot = {
+        type = "function",
+        shortdesc = "Get Annot object.",
+        arguments = {
+          {type = "Annots", name = "annots", optional = false, },
+          {type = "integer", name = "integer", optional = false, },
+        },
+        returnvalues = {
+          {type = "Annot", name = "var", optional = false, },
+        },
+      },
+    },
+    Array = {
+      incRef = {
+        type = "function",
+        shortdesc = "Increment reference count to Array.",
+        arguments = {
+          {type = "Array", name = "array", optional = false, },
+        },
+        returnvalues = {
+        },
+      },
+      decRef = {
+        type = "function",
+        shortdesc = "Decrement reference count to Array.",
+        arguments = {
+          {type = "Array", name = "array", optional = false, },
+        },
+        returnvalues = {
+        },
+      },
+      getLength = {
+        type = "function",
+        shortdesc = "Get Array length.",
+        arguments = {
+          {type = "Array", name = "array", optional = false, },
+        },
+        returnvalues = {
+          {type = "integer", name = "var", optional = false, },
+        },
+      },
+      add = {
+        type = "function",
+        shortdesc = "Add Object to Array.",
+        arguments = {
+          {type = "Array", name = "array", optional = false, },
+          {type = "Object", name = "object", optional = false, },
+        },
+        returnvalues = {
+        },
+      },
+      get = {
+        type = "function",
+        shortdesc = "Get Object from Array.",
+        arguments = {
+          {type = "Array", name = "array", optional = false, },
+          {type = "integer", name = "integer", optional = false, },
+        },
+        returnvalues = {
+          {type = "Object", name = "var", optional = false, },
+        },
+      },
+      getNF = {
+        type = "function",
+        shortdesc = "Get Object from Array, not resolving indirection.",
+        arguments = {
+          {type = "Array", name = "array", optional = false, },
+          {type = "integer", name = "integer", optional = false, },
+        },
+        returnvalues = {
+          {type = "Object", name = "var", optional = false, },
+        },
+      },
+      getString = {
+        type = "function",
+        shortdesc = "Get String from Array.",
+        arguments = {
+          {type = "Array", name = "array", optional = false, },
+          {type = "integer", name = "integer", optional = false, },
+        },
+        returnvalues = {
+          {type = "string", name = "var", optional = false, },
+        },
+      },
+    },
   }
 }
   
