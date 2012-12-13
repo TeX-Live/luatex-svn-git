eb3e334f934b5a3752ce960c28409b1f5d9572c4
Martin Schröder <martin@oneiros.de>
2012-12-13 18:12:27 +0000

    svn:keywords Copyright
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@4515 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/lua/lpdfscannerlib.cc
+++ source/texk/web2c/luatexdir/lua/lpdfscannerlib.cc
@@ -1,3 +1,22 @@
+/* lcallbacklib.c
+   
+   Copyright 2012 Taco Hoekwater <taco@luatex.org>
+
+   This file is part of LuaTeX.
+
+   LuaTeX is free software; you can redistribute it and/or modify it under
+   the terms of the GNU General Public License as published by the Free
+   Software Foundation; either version 2 of the License, or (at your
+   option) any later version.
+
+   LuaTeX is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with LuaTeX; if not, see <http://www.gnu.org/licenses/>. */
+
 #  include <stdlib.h>
 #  include <stdio.h>
 #  include <stdarg.h>
@@ -31,6 +50,9 @@ extern "C" {
 #  include <Error.h>
 
 #  include <lua/luatex-api.h>
+
+static const char _svn_version[] =
+    "$Id$ $URL$";
 
 #define SCANNER "pdfscanner"
 
