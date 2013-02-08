0d3ca0745a8d29430132e042aa95b77dec8f7339
Hartmut Henkel <hartmut_henkel@gmx.de>
2013-02-08 20:42:52 +0000

    Revert "modify pdf_place_glyph() arg"
    This reverts commit 363d147814f6e6f1ce13412c3018a471aabaea53.
    
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@4575 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/pdf/pdfglyph.h
+++ source/texk/web2c/luatexdir/pdf/pdfglyph.h
@@ -1,6 +1,6 @@
 /* pdfglyph.h
 
-   Copyright 2009-2013 Taco Hoekwater <taco@luatex.org>
+   Copyright 2009 Taco Hoekwater <taco@luatex.org>
 
    This file is part of LuaTeX.
 
@@ -24,7 +24,7 @@
 
 void end_chararray(PDF pdf);
 void end_charmode(PDF pdf);
-void pdf_place_glyph(PDF pdf, halfword p);
+void pdf_place_glyph(PDF pdf, internal_font_number f, int c);
 void pdf_print_charwidth(PDF pdf, internal_font_number f, int i);
 
 #endif
