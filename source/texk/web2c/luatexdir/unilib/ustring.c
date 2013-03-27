884741a30fda20d8583b359a8500bf7514ee0786
Taco Hoekwater <taco@elvenkind.com>
2013-03-27 14:21:33 +0000

    -Wtautological-compare
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@4617 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/unilib/ustring.c
+++ source/texk/web2c/luatexdir/unilib/ustring.c
@@ -685,7 +685,7 @@ return( val );
 
 char *utf8_idpb(char *utf8_text,uint32 ch) {
     /* Increment and deposit character */
-    if ( ch<0 || ch>=17*65536 )
+    if ( ch>=17*65536 )
 return( utf8_text );
 
     if ( ch<=127 )
