a0edf18739a2ba755597c432a87c656406774559
Luigi Scarso <luigi.scarso@gmail.com>
2014-02-10 21:04:26 +0000

    fixed   wrong max upvalues in luajit
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@4790 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/libs/luajit/LuaJIT-2.0.2/src/lj_def.h
+++ source/libs/luajit/LuaJIT-2.0.2/src/lj_def.h
@@ -62,7 +62,7 @@ typedef unsigned int uintptr_t;
 #define LJ_MAX_BCINS	(1<<26)		/* Max. # of bytecode instructions. */
 #define LJ_MAX_SLOTS	250		/* Max. # of slots in a Lua func. */
 #define LJ_MAX_LOCVAR	200		/* Max. # of local variables. */
-#define LJ_MAX_UPVAL	60		/* Max. # of upvalues. */
+#define LJ_MAX_UPVAL	249		/* Max. # of upvalues. */
 
 #define LJ_MAX_IDXCHAIN	100		/* __index/__newindex chain limit. */
 #define LJ_STACK_EXTRA	5		/* Extra stack space (metamethods). */
