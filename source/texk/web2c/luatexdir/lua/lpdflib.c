7d406b550a38b2da46b7771bfefa97f29845c2c9
Taco Hoekwater <taco@elvenkind.com>
2014-06-06 09:11:09 +0000

    fix a crash on query-ing pdf.h / pdf.v
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@5018 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/lua/lpdflib.c
+++ source/texk/web2c/luatexdir/lua/lpdflib.c
@@ -576,8 +576,10 @@ static int getpdf(lua_State * L)
         s =  lua_tostring(L, -1);
         if (lua_key_eq(s,h)) {
             lua_pushnumber(L, static_pdf->posstruct->pos.h);
+	    return 1;
         } else if (lua_key_eq(s,v)) {
             lua_pushnumber(L, static_pdf->posstruct->pos.v);
+	    return 1;
         } else if (
                 lua_key_eq(s,catalog) || lua_key_eq(s,info) || lua_key_eq(s,trailer) || lua_key_eq(s,names) ||
                 lua_key_eq(s,pageattributes) || lua_key_eq(s,pagesattributes) || lua_key_eq(s,pageresources)
@@ -587,9 +589,10 @@ static int getpdf(lua_State * L)
             /* [pdf table] [key] [pdf.data table] */
             lua_replace(L, -3);
             /* [pdf.data table] [key] */
+	    lua_rawget(L, -2);
+	    return 1;
         }
     }
-    lua_rawget(L, -2);
     return 0;
 }
 
