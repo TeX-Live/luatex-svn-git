04d314a729cb0a7fe3bf62a6e1cd4decc3abbdf4
Luigi Scarso <luigi.scarso@gmail.com>
2015-09-09 10:36:08 +0000

    sync with trunk
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@5292 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/lua/lnodelib.c
+++ source/texk/web2c/luatexdir/lua/lnodelib.c
@@ -74,6 +74,8 @@
     a speedup on these functions is not representative for a normal run, where
     much more happens.
 
+    maybe : setnext setprev setfont setchar (only makes sense for some scripts)
+
     todo  : check and optimize the direct function when possible
 
     todo  : once the direct ones are proven we can redefine some of the less
@@ -88,11 +90,6 @@
 
     Todo: as a prelude to lua 5.3 we should use integer instead of number when
     possible. A boring job. We can use the direct variants for testing this.
-
-    Remark: when a direct node is set, and an invalid one is passed (or nil) we
-    normally get zero and that is a predefined glue, so we get a message about
-    assigning to a glue which is fine. What we should do some day is make node
-    zero a dummy so that null then can be used as test.
 
     Hans Hagen, Luigi Scarso (2011-2013)
 
@@ -217,7 +214,7 @@
 /* } */
 
 
-#define nodelib_setattr(L, s, n)     reassign_attribute(n,nodelib_getlist(L,s))
+#define nodelib_setattr(L, s, n)     reassign_attribute(n,nodelib_getlist(L,s)) 
 
 #define nodelib_gettoks(L,a)   tokenlist_from_lua(L)
 
@@ -686,33 +683,6 @@ static int lua_nodelib_direct_getcharacter(lua_State * L)
     return 1;
 }
 
-/* node.getdisc */
-
-
-static int lua_nodelib_direct_getdiscretionary(lua_State * L)
-{
-    halfword n;
-    n = (halfword) lua_tonumber(L, 1);
-    if (n != null) {
-        if (type(n) == disc_node) {
-            nodelib_pushdirect_or_nil(vlink(pre_break(n)));
-            nodelib_pushdirect_or_nil(vlink(post_break(n)));
-            nodelib_pushdirect_or_nil(vlink(no_break(n)));
-            if (lua_isboolean(L, 2)) {
-                if (lua_toboolean(L, 2)) {
-                    nodelib_pushdirect_or_nil(tlink(pre_break(n)));
-                    nodelib_pushdirect_or_nil(tlink(post_break(n)));
-                    nodelib_pushdirect_or_nil(tlink(no_break(n)));
-                    return 6;
-                }
-            }
-            return 3;
-        }
-    }
-    lua_pushnil(L);
-    return 1;
-}
-
 /* node.getlist */
 
 static int lua_nodelib_getlist(lua_State * L)
@@ -901,22 +871,6 @@ static int lua_nodelib_direct_getprev(lua_State * L)
         nodelib_pushdirect_or_nil(alink(p));
     }
     return 1;
-}
-
-/* node.direct.getboth */
-
-static int lua_nodelib_direct_getboth(lua_State * L)
-{
-    halfword p;
-    p = (halfword) lua_tonumber(L, 1);
-    if (p == null) {
-        lua_pushnil(L);
-        lua_pushnil(L);
-    } else {
-        nodelib_pushdirect_or_nil(alink(p));
-        nodelib_pushdirect_or_nil(vlink(p));
-    }
-    return 2;
 }
 
 /* node.subtype */
@@ -3946,7 +3900,7 @@ static void lua_nodelib_do_tostring(lua_State * L, halfword n, const char *tag)
 
     if ((alink(n) != null) && (type(n) != attribute_node))
         snprintf(a, 7, "%6d", (int) alink(n));
-    if (vlink(n) != null)
+    if (vlink(n) != null) 
         snprintf(v, 7, "%6d", (int) vlink(n));
     snprintf(msg, 255, "<%s %s < %6d > %s : %s %d>", tag, a, (int) n, v, node_data[type(n)].name, subtype(n));
     lua_pushstring(L, msg);
@@ -5477,133 +5431,6 @@ static int lua_nodelib_direct_setfield_whatsit(lua_State * L, int n, const char 
     return 0;
 }
 
-static int lua_nodelib_direct_setcharacter(lua_State * L)
-{
-    halfword n;
-    n = (halfword ) lua_tonumber(L, 1);
-    if (n) {
-        if ((type(n) == glyph_node) && (lua_type(L, 2) == LUA_TNUMBER)) {
-            character(n) = (halfword) lua_tointeger(L, 2);
-        }
-    }
-    return 0;
-}
-
-static int lua_nodelib_direct_setnext(lua_State * L)
-{
-    halfword n;
-    n = (halfword ) lua_tonumber(L, 1);
-    if (n) {
-        if (lua_type(L, 2) == LUA_TNUMBER) {
-            vlink(n) = (halfword) lua_tointeger(L, 2);
-        } else {
-            vlink(n) = null;
-        }
-    }
-    return 0;
-}
-
-static int lua_nodelib_direct_setprev(lua_State * L)
-{
-    halfword n;
-    n = (halfword ) lua_tonumber(L, 1);
-    if (n) {
-        if (lua_type(L, 2) == LUA_TNUMBER) {
-            alink(n) = (halfword) lua_tointeger(L, 2);
-        } else {
-            alink(n) = null;
-        }
-    }
-    return 0;
-}
-
-static int lua_nodelib_direct_setboth(lua_State * L)
-{
-    halfword n;
-    n = (halfword ) lua_tonumber(L, 1);
-    if (n) {
-        if (lua_type(L, 2) == LUA_TNUMBER) {
-            alink(n) = (halfword) lua_tointeger(L, 2);
-        } else {
-            alink(n) = null;
-        }
-        if (lua_type(L, 3) == LUA_TNUMBER) {
-            vlink(n) = (halfword) lua_tointeger(L, 3);
-        } else {
-            vlink(n) = null;
-        }
-    }
-    return 0;
-}
-
-static int lua_nodelib_direct_setlink(lua_State * L)
-{
-    halfword a, b;
-    if (lua_type(L, 1) == LUA_TNUMBER) {
-        a = (halfword ) lua_tonumber(L, 1);
-        if (lua_type(L, 2) == LUA_TNUMBER) {
-            b = (halfword ) lua_tonumber(L, 2);
-            vlink(a) = b;
-            alink(b) = a;
-        } else {
-            vlink(a) = null;
-        }
-    } else if (lua_type(L, 2) == LUA_TNUMBER) {
-        b = (halfword ) lua_tonumber(L, 2);
-        alink(b) = null;
-    }
-    return 0;
-}
-
-static int lua_nodelib_direct_is_char(lua_State * L)
-{
-    halfword n;
-    halfword f;
-    n = (halfword ) lua_tointeger(L, 1);
-    if ((type(n) == glyph_node) && (subtype(n) < 256)) {
-        f = (halfword ) lua_tointeger(L, 2);
-        if (f && f == font(n)) {
-            lua_pushboolean(L,1);
-            return 1;
-        }
-    }
-    lua_pushboolean(L,0);
-    return 1;
-}
-
-static int lua_nodelib_direct_setdiscretionary(lua_State * L)
-{
-    int t ;
-    halfword n;
-    n = (halfword ) lua_tonumber(L, 1);
-    if (type(n) == disc_node) {
-        t = lua_gettop(L) ;
-        if (t > 1) {
-            set_disc_field(pre_break(n), lua_tointeger(L,2));
-            if (t > 2) {
-                set_disc_field(post_break(n), lua_tointeger(L,3));
-                if (t > 3) {
-                    set_disc_field(no_break(n), lua_tointeger(L,4));
-                    if (t > 4) {
-                        subtype(n) = (quarterword) lua_tointeger(L,5);
-                    }
-                } else {
-                    set_disc_field(no_break(n), null);
-                }
-            } else {
-                set_disc_field(post_break(n), null);
-                set_disc_field(no_break(n), null);
-            }
-        } else {
-            set_disc_field(pre_break(n), null);
-            set_disc_field(post_break(n), null);
-            set_disc_field(no_break(n), null);
-        }
-    }
-    return 0;
-}
-
-
 static int lua_nodelib_direct_setfield(lua_State * L)
 {
     halfword n;
@@ -6464,20 +6291,17 @@ static const struct luaL_Reg direct_nodelib_f[] = {
     {"free", lua_nodelib_direct_free},
     {"getbox", lua_nodelib_direct_getbox},
     {"getchar", lua_nodelib_direct_getcharacter},
-    {"getdisc", lua_nodelib_direct_getdiscretionary},
     {"getfield", lua_nodelib_direct_getfield},
     {"getfont", lua_nodelib_direct_getfont},
     {"getid", lua_nodelib_direct_getid},
     {"getnext", lua_nodelib_direct_getnext},
     {"getprev", lua_nodelib_direct_getprev},
-    {"getboth", lua_nodelib_direct_getboth},
     {"getlist", lua_nodelib_direct_getlist},
     {"getleader", lua_nodelib_direct_getleader},
     {"getsubtype", lua_nodelib_direct_getsubtype},
     {"has_glyph", lua_nodelib_direct_has_glyph},
     {"has_attribute", lua_nodelib_direct_has_attribute},
     {"has_field", lua_nodelib_direct_has_field},
-    {"is_char", lua_nodelib_direct_is_char},
     {"hpack", lua_nodelib_direct_hpack},
  /* {"id", lua_nodelib_id}, */                                /* no node argument */
     {"insert_after", lua_nodelib_direct_insert_after},
@@ -6499,12 +6323,6 @@ static const struct luaL_Reg direct_nodelib_f[] = {
     {"set_attribute", lua_nodelib_direct_set_attribute},
     {"setbox", lua_nodelib_direct_setbox},
     {"setfield", lua_nodelib_direct_setfield},
-    {"setchar", lua_nodelib_direct_setcharacter},
-    {"setdisc", lua_nodelib_direct_setdiscretionary},
-    {"setnext", lua_nodelib_direct_setnext},
-    {"setprev", lua_nodelib_direct_setprev},
-    {"setboth", lua_nodelib_direct_setboth},
-    {"setlink", lua_nodelib_direct_setlink},
     {"slide", lua_nodelib_direct_slide},
  /* {"subtype", lua_nodelib_subtype}, */                      /* no node argument */
     {"tail", lua_nodelib_direct_tail},
