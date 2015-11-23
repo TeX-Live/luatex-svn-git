2c2cdd454aa8af373bdfe3cfbdf92c4606eda740
Luigi Scarso <luigi.scarso@gmail.com>
2015-11-23 19:12:52 +0000

    fixed revision to 0.86.0 (HH)
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@5563 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -27,7 +27,7 @@
 #define TeX
 
 int luatex_svn = luatex_svn_revision*0 - 1; /* for the moment we do this, later we will drop luatex_svn completely */
-int luatex_version = 90;        /* \.{\\luatexversion}  */
+int luatex_version = 86;        /* \.{\\luatexversion}  */
 int luatex_revision = '0';      /* \.{\\luatexrevision}  */
 int luatex_date_info = 2015111900;     /* the compile date is now hardwired */
 const char *luatex_version_string = "beta-0.90.0";
