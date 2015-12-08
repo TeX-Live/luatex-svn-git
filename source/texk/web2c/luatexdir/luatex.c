29b27a8af3d2a7f1079c91fa6bf9b2477940d800
Luigi Scarso <luigi.scarso@gmail.com>
2015-12-08 22:45:18 +0000

    prepare version  0.86.0
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@5611 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -30,7 +30,7 @@
 
 int luatex_version = 86;        /* \.{\\luatexversion}  */
 int luatex_revision = '0';      /* \.{\\luatexrevision}  */
-int luatex_date_info = 2015111900;     /* the compile date is now hardwired */
+int luatex_date_info = 2015120800;     /* the compile date is now hardwired */
 const char *luatex_version_string = "beta-0.86.0";
 const char *engine_name = my_name;     /* the name of this engine */
 
