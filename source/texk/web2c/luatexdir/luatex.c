c1f28a649c096ed35fb22f7c63c0e602df8deb9f
Luigi Scarso <luigi.scarso@gmail.com>
2016-01-11 11:22:28 +0000

    sync with experimental
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@5696 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -29,9 +29,9 @@
 #define TeX
 
 int luatex_version = 87;        /* \.{\\luatexversion}  */
-int luatex_revision = '1';      /* \.{\\luatexrevision}  */
-int luatex_date_info = 2015122000;     /* the compile date is now hardwired */
-const char *luatex_version_string = "beta-0.87.1";
+int luatex_revision = '2';      /* \.{\\luatexrevision}  */
+int luatex_date_info = 2016011100;     /* the compile date is now hardwired */
+const char *luatex_version_string = "beta-0.87.2";
 const char *engine_name = my_name;     /* the name of this engine */
 
 #include <kpathsea/c-ctype.h>
