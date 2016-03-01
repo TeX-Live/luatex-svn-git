84b411e9a25dbb0be04184494f798498f1216e10
Luigi Scarso <luigi.scarso@gmail.com>
2016-03-01 19:01:27 +0000

    several left-over code of oldtoken removed (HH).
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@5818 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/lua/luatex-api.h
+++ source/texk/web2c/luatexdir/lua/luatex-api.h
@@ -128,7 +128,6 @@ extern int luaopen_vf(lua_State * L);
 extern int font_to_lua(lua_State * L, int f);
 extern int font_from_lua(lua_State * L, int f); /* return is boolean */
 
-extern int luaopen_oldtoken(lua_State * L);
 extern int luaopen_token(lua_State * L);
 extern void tokenlist_to_lua(lua_State * L, int p);
 extern void tokenlist_to_luastring(lua_State * L, int p);
@@ -218,7 +217,6 @@ extern void init_tex_table(lua_State * L);
 extern int tex_table_id;
 extern int pdf_table_id;
 extern int token_table_id;
-extern int oldtoken_table_id;
 extern int node_table_id;
 extern int main_initialize(void);
 
@@ -596,7 +594,6 @@ make_lua_key(lua);\
 make_lua_key(lua_functions);\
 make_lua_key(luatex);\
 make_lua_key(luatex_node);\
-make_lua_key(luatex_oldtoken);\
 make_lua_key(luatex_token);\
 make_lua_key(mLTL);\
 make_lua_key(mRTT);\
@@ -935,7 +932,6 @@ init_lua_key(lua);\
 init_lua_key(lua_functions);\
 init_lua_key(luatex);\
 init_lua_key(luatex_node);\
-init_lua_key(luatex_oldtoken);\
 init_lua_key(luatex_token);\
 init_lua_key(marginkern);\
 init_lua_key(mark);\
@@ -1319,7 +1315,6 @@ use_lua_key(lua);
 use_lua_key(lua_functions);
 use_lua_key(luatex);
 use_lua_key(luatex_node);
-use_lua_key(luatex_oldtoken);
 use_lua_key(luatex_token);
 use_lua_key(mLTL);
 use_lua_key(mRTT);
