dd1ea34e572c12f370b9962542a81292a3975e32
Luigi Scarso <luigi.scarso@gmail.com>
2016-03-18 17:30:59 +0000

    fixed beta-0.80.0.1 version string
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/tags/beta-0.80.0.1@5863 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -26,11 +26,11 @@
 
 #define TeX
 
-int luatex_svn = 5238;
+int luatex_svn = 5863;
 int luatex_version = 80;        /* \.{\\luatexversion}  */
 int luatex_revision = '0';      /* \.{\\luatexrevision}  */
-int luatex_date_info = 2015051900;     /* the compile date is now hardwired */
-const char *luatex_version_string = "beta-0.80.0";
+int luatex_date_info = 2016031800;     /* the compile date is now hardwired */
+const char *luatex_version_string = "beta-0.80.0.1";
 const char *engine_name = my_name;     /* the name of this engine */
 
 #include <kpathsea/c-ctype.h>
