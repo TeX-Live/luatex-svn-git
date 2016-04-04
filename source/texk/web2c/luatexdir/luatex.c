3d6c96418cd0eeed5c70f3226ff57932f48cc5b3
Luigi Scarso <luigi.scarso@gmail.com>
2016-04-04 13:30:05 +0000

    preparing beta-0.95.0 (HH)
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@5906 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -30,7 +30,7 @@
 
 int luatex_version = 95;        /* \.{\\luatexversion}  */
 int luatex_revision = '0';      /* \.{\\luatexrevision}  */
-int luatex_date_info = 2016033010;     /* the compile date is now hardwired */
+int luatex_date_info = 2016040415;     /* the compile date is now hardwired */
 const char *luatex_version_string = "beta-0.95.0";
 const char *engine_name = my_name;     /* the name of this engine */
 
