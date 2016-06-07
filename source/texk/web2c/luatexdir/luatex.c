b8019dff033d0348f95e01971465d8bc24cf8172
Luigi Scarso <luigi.scarso@gmail.com>
2016-06-07 20:49:48 +0000

    fix for self refering glyph in vf font;  version 0.97.0 (HH)
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@6032 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -28,10 +28,10 @@
 
 #define TeX
 
-int luatex_version = 96;        /* \.{\\luatexversion}  */
+int luatex_version = 97;        /* \.{\\luatexversion}  */
 int luatex_revision = '0';      /* \.{\\luatexrevision}  */
-int luatex_date_info = 2016051817;     /* the compile date is now hardwired */
-const char *luatex_version_string = "0.96.0";
+int luatex_date_info = 2016060720;     /* the compile date is now hardwired */
+const char *luatex_version_string = "0.97.0";
 const char *engine_name = my_name;     /* the name of this engine */
 
 #include <kpathsea/c-ctype.h>
