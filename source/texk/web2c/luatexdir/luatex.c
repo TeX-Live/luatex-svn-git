95722bcb5aed9a5f5837383fa467d384378f1ab7
Luigi Scarso <luigi.scarso@gmail.com>
2016-09-23 11:50:43 +0000

    preparation on luatex 1.0
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@6144 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -29,9 +29,9 @@
 #define TeX
 
 int luatex_version = 99;        /* \.{\\luatexversion}  */
-int luatex_revision = '1';      /* \.{\\luatexrevision}  */
+int luatex_revision = '5';      /* \.{\\luatexrevision}  */
 int luatex_date_info = 2016092300;     /* the compile date is now hardwired */
-const char *luatex_version_string = "0.99.1";
+const char *luatex_version_string = "0.99.5";
 const char *engine_name = my_name;     /* the name of this engine */
 
 #include <kpathsea/c-ctype.h>
