d830647ce2d4bede68f9060c119bac82d0bb151d
Luigi Scarso <luigi.scarso@gmail.com>
2017-02-08 09:04:36 +0000

    corrected luaopen_ffi declaration (thanks to A. Kakuto) -- Warning: work in progress!
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@6210 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/lua/luatex-api.h
+++ source/texk/web2c/luatexdir/lua/luatex-api.h
@@ -87,7 +87,9 @@ extern int luaopen_lpeg(lua_State * L);
 extern int luaopen_md5(lua_State * L);
 extern int luatex_md5_lua_open(lua_State * L);
 
-extern int luaopen_ffi(lua_State * L);
+#ifndef LuajitTeX
+ extern int luaopen_ffi(lua_State * L);
+#endif
 
 extern int luaopen_zlib(lua_State * L);
 extern int luaopen_gzip(lua_State * L);
