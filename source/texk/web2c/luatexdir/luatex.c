981b2aa9e2752e52e41b7b4d8984ff95c8f5b945
Luigi Scarso <luigi.scarso@gmail.com>
2017-10-03 20:48:02 +0000

    preparing version 1.06.1
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@6409 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -33,8 +33,8 @@
 */
 
 int luatex_version = 106;
-int luatex_revision = '0';
-const char *luatex_version_string = "1.06.0";
+int luatex_revision = '1';
+const char *luatex_version_string = "1.06.1";
 const char *engine_name = my_name;
 
 #include <kpathsea/c-ctype.h>
--- source/texk/web2c/luatexdir/luatex_svnversion.h
+++ source/texk/web2c/luatexdir/luatex_svnversion.h
@@ -1,1 +1,1 @@
-#define luatex_svn_revision 6408
+#define luatex_svn_revision 6409
