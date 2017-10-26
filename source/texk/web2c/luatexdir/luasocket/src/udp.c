8d00f73f672ec270d4523618cf6af6afabd175c4
Luigi Scarso <luigi.scarso@gmail.com>
2017-10-26 09:08:47 +0000

    exp-lua53: fixed inet_pton inet_ntop for luasocket under mingw
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/exp-lua53@6435 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luasocket/src/udp.c
+++ source/texk/web2c/luatexdir/luasocket/src/udp.c
@@ -43,6 +43,60 @@ static int meth_getfd(lua_State *L);
 static int meth_setfd(lua_State *L);
 static int meth_dirty(lua_State *L);
 
+
+#if defined(__MINGW32__)
+static const char *inet_ntop(int af, const void *src, char *dst, socklen_t cnt)
+{
+        if (af == AF_INET)
+        {
+                struct sockaddr_in in;
+                memset(&in, 0, sizeof(in));
+                in.sin_family = AF_INET;
+                memcpy(&in.sin_addr, src, sizeof(struct in_addr));
+                getnameinfo((struct sockaddr *)&in, sizeof(struct
+sockaddr_in), dst, cnt, NULL, 0, NI_NUMERICHOST);
+                return dst;
+        }
+        else if (af == AF_INET6)
+        {
+                struct sockaddr_in6 in;
+                memset(&in, 0, sizeof(in));
+                in.sin6_family = AF_INET6;
+                memcpy(&in.sin6_addr, src, sizeof(struct in_addr6));
+                getnameinfo((struct sockaddr *)&in, sizeof(struct
+sockaddr_in6), dst, cnt, NULL, 0, NI_NUMERICHOST);
+                return dst;
+        }
+        return NULL;
+}
+
+static int inet_pton(int af, const char *src, void *dst)
+{
+        struct addrinfo hints, *res, *ressave;
+
+        memset(&hints, 0, sizeof(struct addrinfo));
+        hints.ai_family = af;
+
+        if (getaddrinfo(src, NULL, &hints, &res) != 0)
+        {
+                return -1;
+        }
+
+        ressave = res;
+
+        while (res)
+        {
+                memcpy(dst, res->ai_addr, res->ai_addrlen);
+                res = res->ai_next;
+        }
+
+        freeaddrinfo(ressave);
+        return 0;
+}
+
+#endif 
+
+
 /* udp object methods */
 static luaL_Reg udp_methods[] = {
     {"__gc",        meth_close},
