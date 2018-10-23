1efc9cb70f8a05fa22ef2d273e9f171095827ce6
Luigi Scarso <luigi.scarso@gmail.com>
2018-10-23 12:06:37 +0000

    patch for 'Bug in \letcharcode' (thanks to Marcel Krüger)
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@6981 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/tex/maincontrol.c
+++ source/texk/web2c/luatexdir/tex/maincontrol.c
@@ -2698,9 +2698,8 @@ void prefixed_command(void)
                     /*tex |letcharcode| */
                     scan_int();
                     if (cur_val > 0) {
-                        cur_cs = active_to_cs(cur_val, true);
-                        set_token_info(cur_cs, cur_cs + cs_token_flag);
-                        p = cur_cs;
+                        /*tex HH: I need to  do a more extensive test later. */
+                        p = active_to_cs(cur_val, true);
                         do {
                             get_token();
                         } while (cur_cmd == spacer_cmd);
