d95d034d3273638e122918eec53e4783e3a808ed
Luigi Scarso <luigi.scarso@gmail.com>
2018-10-23 22:59:07 +0000

    bump up to 1.09.1
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@6985 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -33,8 +33,8 @@
 */
 
 int luatex_version = 109;
-int luatex_revision = '0';
-const char *luatex_version_string = "1.09.0";
+int luatex_revision = '1';
+const char *luatex_version_string = "1.09.1";
 const char *engine_name = my_name;
 
 #include <kpathsea/c-ctype.h>
