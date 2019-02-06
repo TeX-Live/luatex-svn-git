94d096ec7e7d9cfb5c93e3e5fc37cea060127dda
Luigi Scarso <luigi.scarso@gmail.com>
2019-02-06 00:20:27 +0000

    fix a few token mem consumption issues (HH).
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@7078 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex.c
+++ source/texk/web2c/luatexdir/luatex.c
@@ -33,8 +33,8 @@
 */
 
 int luatex_version = 109;
-int luatex_revision = '2';
-const char *luatex_version_string = "1.09.2";
+int luatex_revision = '3';
+const char *luatex_version_string = "1.09.3";
 const char *engine_name = my_name;
 
 #include <kpathsea/c-ctype.h>
