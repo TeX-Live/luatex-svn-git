4ad5bfb91a33c4b80b140fa2e3b216db531b3d79
Luigi Scarso <luigi.scarso@gmail.com>
2019-04-15 09:17:00 +0000

    sync with TeXLive 2019: final step.
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@7131 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/lib/texmfmp.c
+++ source/texk/web2c/lib/texmfmp.c
@@ -738,16 +738,24 @@ maininit (int ac, string *av)
   parse_options (ac, av);
 #endif
 
-#if IS_pTeX
-  /* In pTeX and friends, texmf.cnf is not recorded in the case of --recorder,
-     because parse_options() is executed after the start of kpathsea due to
-     special initializations. Therefore we record texmf.cnf here. */
+#if IS_pTeX || ((defined(XeTeX) || defined(pdfTeX)) && defined(WIN32))
+  /* In pTeX and friends, or in WIN32, texmf.cnf is not recorded in
+     the case of --recorder, because parse_options() is executed
+     after the start of kpathsea due to special initializations.
+     Therefore we record texmf.cnf here. */
   if (recorder_enabled) {
-    string p = kpse_find_file ("texmf.cnf", kpse_cnf_format, 0);
-    if (p)
-      recorder_record_input (p);
+    string *p = kpse_find_file_generic ("texmf.cnf", kpse_cnf_format, 0, 1);
+    if (p && *p) {
+      string *pp = p;
+      while (*p) {
+        recorder_record_input (*p);
+        free (*p);
+        p++;
+      }
+      free (pp);
+    }
   }
-#endif
+#endif /* IS_pTeX || (...) */
 
   /* If -progname was not specified, default to the dump name.  */
   if (!user_progname)
--- source/texk/web2c/luatexdir/luatex_svnversion.h
+++ source/texk/web2c/luatexdir/luatex_svnversion.h
@@ -1,1 +1,1 @@
-#define luatex_svn_revision 7130
+#define luatex_svn_revision 7131
