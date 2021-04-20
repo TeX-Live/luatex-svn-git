7492c95f74f952993a5dcb3b7c95a03c3c3543fa
Luigi Scarso <luigi.scarso@gmail.com>
2021-04-20 12:27:58 +0000

    \mathdefaultsmode: different radical style defaults (HH)
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@7435 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex_svnversion.h
+++ source/texk/web2c/luatexdir/luatex_svnversion.h
@@ -1,1 +1,1 @@
-#define luatex_svn_revision 7434
+#define luatex_svn_revision 7435
--- source/texk/web2c/luatexdir/tex/equivalents.h
+++ source/texk/web2c/luatexdir/tex/equivalents.h
@@ -309,8 +309,9 @@ the |number_regs| \.{\\dimen} registers.
 #  define suppress_primitive_error_code 116
 #  define fixup_boxes_code 117
 #  define glyph_dimensions_code 118
+#  define math_defaults_mode_code 119                                   /* > 0 : latex preferences */
 
-#  define math_option_code 119
+#  define math_option_code 120
 
 #  define mathoption_int_base_code (math_option_code+1)                 /* one reserve */
 #  define mathoption_int_last_code (mathoption_int_base_code+8)
@@ -811,6 +812,7 @@ extern halfword last_cs_name;
 
 #define fixup_boxes_par                    int_par(fixup_boxes_code)
 #define glyph_dimensions_par               int_par(glyph_dimensions_code)
+#define math_defaults_mode_par             int_par(math_defaults_mode_code)
 
 /* */
 
