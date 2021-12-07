265bc54d8dd4a286cb7af43f2a71bf5187f77971
Luigi Scarso <luigi.scarso@gmail.com>
2021-12-07 15:16:45 +0000

    Fixed a typo in maincontrol.c (thanks to Hironobu Yamashita)
    git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/branches/experimental@7461 0b2b3880-5936-4365-a048-eb17d2e5a6bf

--- source/texk/web2c/luatexdir/luatex_svnversion.h
+++ source/texk/web2c/luatexdir/luatex_svnversion.h
@@ -1,1 +1,1 @@
-#define luatex_svn_revision 7459
+#define luatex_svn_revision 7461
--- source/texk/web2c/luatexdir/tex/maincontrol.c
+++ source/texk/web2c/luatexdir/tex/maincontrol.c
@@ -1289,7 +1289,7 @@ void report_illegal_case(void)
     you_cant();
     help4(
         "Sorry, but I'm not programmed to handle this case;",
-        "I'll just pretend that you didn''t ask for it.",
+        "I'll just pretend that you didn't ask for it.",
         "If you're in the wrong mode, you might be able to",
         "return to the right one by typing `I}' or `I$' or `I\\par'."
     );
